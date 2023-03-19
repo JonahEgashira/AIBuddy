a7a15c8e9614f846e2c76ee4074271e2379df33e
JonahEgashira <jonahega@gmail.com>
2023-03-19 15:12:14 +0900

    Initial Commit

---
+++ AIBuddy/AIBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIBuddyApp.swift
+//  AIBuddy
+//
+//  Created by Jonah Egashira on 2023/03/19.
+//
+
+import SwiftUI
+
+@main
+struct AIBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
